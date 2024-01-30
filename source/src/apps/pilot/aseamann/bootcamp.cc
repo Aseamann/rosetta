096aef48fbe7bc4050c1ec02e3730f59f23e1860
Aseamann <aseamann@unomaha.edu>
2024-01-29 21:16:06 -0600

    Implementation of MonteCarlo mover

--- source/src/apps/pilot/aseamann/bootcamp.cc
+++ source/src/apps/pilot/aseamann/bootcamp.cc
@@ -18,6 +18,8 @@
 # include <core/scoring/ScoreFunctionFactory.hh>  // Needed for scoring
 # include <core/scoring/ScoreFunction.hh> // Needed for scoring 
 # include <core/scoring/Energies.hh> // Showing energies from scoring
+# include <numeric/random/random.hh>  // For random number
+# include <protocols/moves/MonteCarlo.hh>  // For montecarlo
 
 int main( int argc, char ** argv ) {
     devel::init( argc, argv );
@@ -31,6 +33,8 @@ int main( int argc, char ** argv ) {
 
     // Read in pdb
     core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1] );
+    // Collect length of residues
+    int N = mypose->size();
 
     // Set score funciton
     core::scoring::ScoreFunctionOP sfxn = core::scoring::get_score_function();
@@ -39,6 +43,32 @@ int main( int argc, char ** argv ) {
     // Print score
     // mypose->energies().show( std::cout );
     std::cout << "Score Results is: " << score << std::endl;
+
+    // Setup monte carlo object
+    core::Real temp = 1.0;
+    protocols::moves::MonteCarloOP mc ( new protocols::moves::MonteCarlo( *mypose, *sfxn, temp ) );
+
+    int loop_count = 10;
+    for( int i = 0 ; i < loop_count; i++ ) {
+        // Determine random residue
+        double uniform_random_number = numeric::random::uniform();
+        core::Size randres = static_cast< core::Size > (uniform_random_number * N + 1 );
+        // Collect random numbers to adjust phi/psi
+        core::Real pert1 = numeric::random::gaussian();
+        core::Real pert2 = numeric::random::gaussian();
+        // Collect original phi/psi
+        core::Real orig_phi = mypose->phi( randres );
+        core::Real orig_psi = mypose->psi( randres );
+        // Set random phi/psi
+        mypose->set_phi( randres, orig_phi + pert1 );
+        mypose->set_psi( randres, orig_psi + pert2 );
+
+        mc->boltzmann( *mypose );
+    }
+
+    // Check pose
+    core::Real score2 = sfxn->score( *mypose );
+    std::cout << "Updated Score: " << score2 << std::endl;
 
     return 0;
 }
