a6b03a435eaecbaf8bbb00921bd79802a4026b79
Aseamann <aseamann@unomaha.edu>
2024-01-31 16:57:00 -0600

    Fold Tree working for FoldTreeFromSS

--- source/test/protocols/bootcamp/FoldTreeFromSS.cxxtest.hh
+++ source/test/protocols/bootcamp/FoldTreeFromSS.cxxtest.hh
@@ -20,18 +20,22 @@
 
 // Utility headers
 #include <utility/vector1.hh>
+# include <basic/Tracer.hh>  // Output
 
 /// Project headers
 #include <core/types.hh>
 #include <core/pose/Pose.hh>
 #include <core/kinematics/FoldTree.hh>
 #include <core/scoring/dssp/Dssp.hh>
-#include <core/kinematics/edge/Edge.hh>
+#include <core/kinematics/Edge.hh>
 
 // C++ headers
 
 //Auto Headers
 #include <core/pack/dunbrack/DunbrackRotamer.hh>
+
+// Setup tracer cout
+static basic::Tracer TR( "apps.pilot.aseamann" );
 
 // -------------- Implemented Code --------------- //
 utility::vector1< std::pair< core::Size, core::Size > >
@@ -66,19 +70,8 @@ identify_secondary_structure_spans( std::string const & ss_string )
   return ss_boundaries;
 }
 
-
-std::string
-fold_tree_from_ss( core::pose::Pose const & pose )
-{
-	// Setup DSSP Call
-	std::string dssp_str = core::scoring::dssp::Dssp( pose ).get_dssp_secstruct();
-	core::kinematics::FoldTreeOP ft_dssp = from_tree_from_dssp( dssp_str );
-
-	return result;
-}
-
 core::kinematics::FoldTreeOP
-from_tree_from_dssp( std::string const & dssp_string )
+fold_tree_from_dssp_string( std::string const & dssp_string )
 {
 	// Setup FoldTree
 	core::kinematics::FoldTreeOP ft( new core::kinematics::FoldTree() );
@@ -87,42 +80,81 @@ from_tree_from_dssp( std::string const & dssp_string )
 	utility::vector1< std::pair< core::Size, core::Size > > ss_boundaries = identify_secondary_structure_spans( dssp_string );
 
 	// Loop over pairs and add to fold tree
-	utility::vector1< core:Size > mid_points;
+	utility::vector1< core::Size > w_loop_mid_points;
 	for ( core::Size ii = 1; ii <= ss_boundaries.size(); ++ii ) {
 		// Get start and end of ss element
 		core::Size start = ss_boundaries[ ii ].first;
 		core::Size end = ss_boundaries[ ii ].second;
-		core::Size mid = ( end - start ) / 2;
+		core::Size mid = ( ( end - start ) / 2 ) + start;
+		// Get the midpoints of the loops based on the last ss end and current ss start
+		if ( ii > 1 ) {
+			core::Size w_loop_mid = ( ( start - ss_boundaries[ ii - 1 ].second ) / 2 ) + ss_boundaries[ ii - 1 ].second;
+			w_loop_mid_points.push_back( w_loop_mid );
+		} 
 		// Add mid point to fold tree
-		mid_points.push_back( mid );
+		w_loop_mid_points.push_back( mid );
 	}
 
 	// Build fold tree, starting at position 1 and to each mid point
 	core::Size jump = 1;
-	for ( core::Size ii = 1; ii <= mid_points.size(); ++ii ) {
-		if ( ii == 1 ) {
-			// Add edge from 1 to first mid point
-			ft->add_edge( 1, mid_points[ ii ], core::kinematics::Edge::PEPTIDE );
-		} else {
-			// Add in first jump, but fill in the remainder of peptide edges
-			ft->add_edge( mid_points[ ii ], mid_points[ ii ], jump++ );
-			if ( jump == 1 ) {
-				// Only need to fill in downstream peptide edges
-				ft->add_edge( mid_points[ ii ], ss_boundaries[ jump ].second, core::kinematics::Edge::PEPTIDE );
+	core::Size count_ss = 1;
+	bool ss = true;
+	core::Size current_mid;
+	for ( core::Size ii = 1; ii <= w_loop_mid_points.size(); ++ii ) {
+
+		current_mid = w_loop_mid_points[ ii ];
+
+		// Check to see if current jump is within a ss or not, update bool ss
+		if ( ii != 1 ) {
+			if ( (ss_boundaries[count_ss - 1].first <= current_mid) && (current_mid >= ss_boundaries[count_ss - 1].second) ) {
+				ss = true;
 			} else {
-				// Need to fill in both upstream and downstream peptide edges
-				ft->add_edge( ss_boundaries[ jump + 1 ].first, mid_points[ ii ], core::kinematics::Edge::PEPTIDE );
-				ft->add_edge( mid_points[ ii ], ss_boundaries[ jump ].second, core::kinematics::Edge::PEPTIDE );
+				ss = false;
 			}
 		}
-		// After performing jumps, fill in upstream and downstream of last jump
-		if ( ii == mid_points.size() ) {
-			ft->add_edge( ss_boundaries[ ii + 1 ].first, mid_points[ ii ], core::kinematics::Edge::PEPTIDE );
-			ft->add_edge( mid_points[ ii ], ss_boundaries[ ii ].second, core::kinematics::Edge::PEPTIDE );
+
+		if ( ii == 1 ) {
+			// Add edge from 1 to first mid point
+			ft->add_edge( current_mid, 1, core::kinematics::Edge::PEPTIDE );
+			// Add edge from start node to end of ss element
+			ft->add_edge( current_mid, ss_boundaries[ count_ss++ ].second, core::kinematics::Edge::PEPTIDE );
+		} else if ( ss ) {
+			// Add edge from first mid point to current mid point
+			ft->add_edge( w_loop_mid_points[ 1 ], current_mid, jump++ );
+			// Add current mid point to start/end of ss element
+			ft->add_edge( current_mid, ss_boundaries[ count_ss - 1 ].second + 1 , core::kinematics::Edge::PEPTIDE );
+			if (ii != w_loop_mid_points.size()) {
+				ft->add_edge( current_mid, ss_boundaries[ count_ss ].first - 1 , core::kinematics::Edge::PEPTIDE );
+			} else {
+				ft->add_edge( current_mid, dssp_string.size(), core::kinematics::Edge::PEPTIDE );
+			}
+			// Update count_ss
+			count_ss++;
+		} else {
+			// Add edge from first mid point to current mid point
+			ft->add_edge( w_loop_mid_points[ 1 ], current_mid, jump++ );
+			// Add current mid point to start/end of ss element
+			ft->add_edge( current_mid, ss_boundaries[ count_ss - 1 ].first, core::kinematics::Edge::PEPTIDE );
+			if (ii != w_loop_mid_points.size()) {
+				ft->add_edge( current_mid, ss_boundaries[ count_ss - 1 ].second, core::kinematics::Edge::PEPTIDE );
+			} else {
+				ft->add_edge( current_mid, dssp_string.size(), core::kinematics::Edge::PEPTIDE );
+			}
 		}
 	}
 
 	return ft;
+}
+
+core::kinematics::FoldTreeOP
+fold_tree_from_ss( core::pose::Pose const & pose )
+{
+	// Setup DSSP Call
+	std::string dssp_str = core::scoring::dssp::Dssp( pose ).get_dssp_secstruct();
+	// Call fold_tree_from_dssp_string
+	core::kinematics::FoldTreeOP ft_dssp = fold_tree_from_dssp_string( dssp_str );
+
+	return ft_dssp;
 }
 
 
@@ -240,6 +272,25 @@ public:
 		std::pair< core::Size, core::Size > pair39 = std::make_pair( 44, 51 );
 		test_pairs3.push_back( pair39 );
 
+		// Loop over pairs and compare
+		for ( core::Size ii = 1; ii <= ss_boundaries3.size(); ++ii ) {
+			TS_ASSERT_EQUALS( ss_boundaries3[ ii ].first, test_pairs3[ ii ].first );
+			TS_ASSERT_EQUALS( ss_boundaries3[ ii ].second, test_pairs3[ ii ].second );
+		}
+
+	}
+
+	// Test to perform running fold_tree_from_dssp_string
+	void test_fold_tree_from_dssp_string() {
+		// Setup string
+		std::string ss_string = "   EEEEEEE    EEEEEEE         EEEEEEEEE    EEEEEEEEEE   HHHHHH         EEEEEEEEE         EEEEE     ";
+		// Setup fold tree
+		core::kinematics::FoldTreeOP ft = fold_tree_from_dssp_string( ss_string );
+		// Fold tree to string
+		std::string ft_string = ft->to_string();
+		TR << "Fold Tree Test Fold Tree From Dssp:\n" << ft_string << std::endl;
+		// Validate fold tree, num edges
+		TS_ASSERT_EQUALS( ft->size(), 38 );
 	}
 
 };
